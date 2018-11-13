b891557e7d997c34215b7b487e44c6735a8de2cb
PaperPlane01 <dagfortnewage@gmail.com>
2018-11-13 16:08:13 +0600

    Fixed: org.equinox.mapper.CreateGlobalBlockingDTOToGlobalBlockingMapper#setFields method were not being invoked

--- src/main/java/org/equinox/mapper/CreateGlobalBlockingDTOToGlobalBlockingMapper.java
+++ src/main/java/org/equinox/mapper/CreateGlobalBlockingDTOToGlobalBlockingMapper.java
@@ -1,14 +1,15 @@
 package org.equinox.mapper;
 
-import org.mapstruct.BeanMapping;
-import org.mapstruct.Mapper;
-import org.mapstruct.MappingTarget;
 import org.equinox.exception.EntityNotFoundExceptionFactory;
 import org.equinox.model.domain.GlobalBlocking;
 import org.equinox.model.domain.User;
 import org.equinox.model.dto.CreateGlobalBlockingDTO;
 import org.equinox.repository.UserRepository;
 import org.equinox.security.AuthenticationFacade;
+import org.mapstruct.BeanMapping;
+import org.mapstruct.BeforeMapping;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @Mapper
@@ -25,6 +26,7 @@ public abstract class CreateGlobalBlockingDTOToGlobalBlockingMapper {
     @BeanMapping(resultType = GlobalBlocking.class)
     public abstract GlobalBlocking map(CreateGlobalBlockingDTO createGlobalBlockingDTO);
 
+    @BeforeMapping
     protected void setFields(CreateGlobalBlockingDTO createGlobalBlockingDTO,
                              @MappingTarget GlobalBlocking globalBlocking) {
         globalBlocking.setBlockedUser(userRepository.findById(createGlobalBlockingDTO.getBlockedUserId())
