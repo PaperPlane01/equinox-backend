0a5170d63117125a09c07689c2270d6bdc1799b5
PaperPlane01 <dagfortnewage@gmail.com>
2019-06-08 19:58:24 +0600

    Fixed: likes to deleted blog posts were included in response of /api/users/:id/blog-post-likes endpoint

--- src/main/java/aphelion/repository/BlogPostLikeRepository.java
+++ src/main/java/aphelion/repository/BlogPostLikeRepository.java
@@ -5,6 +5,8 @@ import aphelion.model.domain.BlogPostLike;
 import aphelion.model.domain.User;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -15,7 +17,10 @@ public interface BlogPostLikeRepository extends JpaRepository<BlogPostLike, Long
     void delete(BlogPostLike blogPostLike);
     Optional<BlogPostLike> findByBlogPostAndUser(BlogPost blogPost, User user);
     List<BlogPostLike> findByBlogPost(BlogPost blogPost, Pageable pageable);
-    List<BlogPostLike> findByUser(User user, Pageable pageable);
     Optional<BlogPostLike> findByBlogPostIdAndUserId(Long blogPostId, Long userId);
     int countByBlogPost(BlogPost blogPost);
+
+    @Query("select blogPostLike from BlogPostLike blogPostLike " +
+            "where (blogPostLike.user = :#{#user} and not blogPostLike.blogPost.deleted = true)")
+    List<BlogPostLike> findByUser(@Param("user") User user, Pageable pageable);
 }
